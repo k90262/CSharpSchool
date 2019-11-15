c5b1330e21d8578b583a7b93aa669eff369c3b2b
Bill Ho <k90262xp@hotmail.com>
2019-11-15 16:14:21 +0800

    [Lab02][Ex2] Validating Student Information

--- School/StudentForm.xaml.cs
+++ School/StudentForm.xaml.cs
@@ -20,10 +20,32 @@ namespace School
         // If the user clicks OK to save the Student details, validate the information that the user has provided
         private void ok_Click(object sender, RoutedEventArgs e)
         {
-            // TODO: Exercise 2: Task 2a: Check that the user has provided a first name
-            // TODO: Exercise 2: Task 2b: Check that the user has provided a last name
-            // TODO: Exercise 2: Task 3a: Check that the user has entered a valid date for the date of birth
-            // TODO: Exercise 2: Task 3b: Verify that the student is at least 5 years old
+            // Check that the user has provided a first name
+            if (String.IsNullOrEmpty(this.firstName.Text))
+            {
+                MessageBox.Show("The student must has a first name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            // Check that the user has provided a last name
+            if (String.IsNullOrEmpty(this.lastName.Text))
+            {
+                MessageBox.Show("The student must has a last name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            // Check that the user has entered a valid date for the date of birth
+            DateTime result;
+            if (!DateTime.TryParse(this.dateOfBirth.Text, out result))
+            {
+                MessageBox.Show("The date of birth must be a valid date", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            // Verify that the student is at least 5 years old
+            TimeSpan age = DateTime.Now.Subtract(result);
+            if (age.Days / 365.25 < 5)
+            {
+                MessageBox.Show("The student must be at least 5 years old", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
             // Indicate that the data is valid
             this.DialogResult = true;
